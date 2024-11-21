3b439ffd510a730103cbc8623bd0372ffe918f19
Doris26 <zouyujun526@gmail.com>
2024-11-21 21:50:22 +0000

    fix value error

--- MaxText/train.py
+++ MaxText/train.py
@@ -83,7 +83,7 @@ def validate_train_config(config):
   if not config.base_output_directory.startswith("gs://"):
     max_logging.log("WARNING: 'base_output_directory' might be pointing your local file system")
   assert config.steps > 0, "You must set steps or learning_rate_schedule_steps to a positive integer."
-  if config.tolerance and (config.tolerance > 1.0 or config.tolerance < 0.0):
+  if "tolerance" in config.__dict__ and (config.tolerance > 1.0 or config.tolerance < 0.0):
     max_logging.log("WARNING: 'tolerance: allowed percentage of non-sharded parameters' should be between 0.0 and 1.0")
   if config.quantization == "fp8":
     # pylint: disable=line-too-long
@@ -539,7 +539,7 @@ def setup_train_loop(config):
   record_goodput(recorder, config, recorder.record_tpu_init_end_time if recorder else None)
   record_goodput(recorder, config, recorder.record_training_preparation_start_time if recorder else None)
   data_iterator, eval_data_iterator = create_data_iterator(config, mesh)
-  tolerance = config.tolerance if config.tolerance else _DEFAULT_TOLERANCE
+  tolerance = config.tolerance if "tolerance" in config.__dict__ else _DEFAULT_TOLERANCE
 
   state, state_mesh_annotations, data_iterator = max_utils.setup_training_state(
       model, data_iterator, tx, config, init_rng, mesh, checkpoint_manager
