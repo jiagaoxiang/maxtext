9c399402aba410330cf11d8e0a66cbed6d580f2f
jiagaoxiang <jiagaoxiang@gmail.com>
2024-12-04 21:53:18 +0000

    update

--- MaxText/layers/llama3_2.py
+++ MaxText/layers/llama3_2.py
@@ -481,48 +481,6 @@ class VisionPrecomputedPositionEmbedding(nn.Module):
 
         return hidden_state
 
-# class VisionEmbeddings(nn.Module):
-#   config: Config
-
-#   def setup(self):
-#     embed_dim = self.config.hidden_size
-#     image_size = self.config.image_size
-#     patch_size = self.config.patch_size
-
-#     self.class_embedding = self.param("class_embedding", jax.nn.initializers.normal(stddev=0.02), (embed_dim,), self.config.weight_dtype)
-#     self.patch_embedding = nn.Conv(
-#         embed_dim,
-#         kernel_size=(patch_size, patch_size),
-#         strides=(patch_size, patch_size),
-#         padding="VALID",
-#         use_bias=False,
-#         dtype=self.config.dtype,
-#         param_dtype=self.config.weight_dtype,
-#         kernel_init=jax.nn.initializers.normal(),
-#     )
-
-#     self.num_patches = (image_size // patch_size) ** 2
-#     num_positions = self.num_patches + 1
-#     self.position_embedding = nn.Embed(
-#         num_positions,
-#         embed_dim,
-#         embedding_init=jax.nn.initializers.normal(),
-#         dtype=self.config.dtype,
-#         param_dtype=self.config.weight_dtype,
-#     )
-#     self.position_ids = jnp.expand_dims(jnp.arange(0, num_positions, dtype="i4"), axis=0)
-
-#   def __call__(self, pixel_values):
-#     patch_embeds = self.patch_embedding(pixel_values)
-#     batch_size, height, width, channels = patch_embeds.shape
-#     patch_embeds = jnp.reshape(patch_embeds, (batch_size, height * width, channels))
-
-#     class_embeds = jnp.expand_dims(self.class_embedding, axis=(0, 1))
-#     class_embeds = jnp.tile(class_embeds, (batch_size, 1, 1))
-#     embeddings = jnp.concatenate([class_embeds, patch_embeds], axis=1)
-#     embeddings = embeddings + self.position_embedding(self.position_ids)
-#     return embeddings
-
 
 class VisionPrecomputedAspectRatioEmbedding(nn.Module):
     config: Config
